ed094c8bbeb8c583bf82a39340c5390f47f977b1
adithyapradeep05 <adithyaapradeep@gmail.com>
2026-05-12 18:41:49 -0700

    ui: replace Table with 21st.dev version

--- frontend/src/components/ui/table.tsx
+++ frontend/src/components/ui/table.tsx
@@ -4,28 +4,54 @@ import { cn } from "@/lib/utils";
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
-    <div className="relative w-full overflow-auto">
+    <div className="w-full overflow-auto min-w-[248px] p-6 rounded-lg relative border border-border bg-muted/20">
       <table ref={ref} className={cn("w-full caption-bottom text-sm", className)} {...props} />
     </div>
   ),
 );
 Table.displayName = "Table";
 
+const TableColgroup = React.forwardRef<HTMLTableColElement, React.ColHTMLAttributes<HTMLTableColElement>>(
+  ({ className, ...props }, ref) => <colgroup ref={ref} className={cn(className)} {...props} />,
+);
+TableColgroup.displayName = "TableColgroup";
+
+const TableCol = React.forwardRef<HTMLTableColElement, React.ColHTMLAttributes<HTMLTableColElement>>(
+  ({ className, ...props }, ref) => <col ref={ref} className={cn(className)} {...props} />,
+);
+TableCol.displayName = "TableCol";
+
 const TableHeader = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
-  ({ className, ...props }, ref) => <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />,
+  ({ className, ...props }, ref) => (
+    <thead ref={ref} className={cn("border-b border-border", className)} {...props} />
+  ),
 );
 TableHeader.displayName = "TableHeader";
 
-const TableBody = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
-  ({ className, ...props }, ref) => (
-    <tbody ref={ref} className={cn("[&_tr:last-child]:border-0", className)} {...props} />
-  ),
-);
+const TableBody = React.forwardRef<
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement> & { striped?: boolean; interactive?: boolean }
+>(({ className, striped, interactive, ...props }, ref) => (
+  <tbody
+    ref={ref}
+    className={cn(
+      "[&_tr:last-child]:border-0",
+      striped && "[&_tr:where(:nth-child(odd))]:bg-muted/30",
+      interactive && "[&_tr:hover]:bg-muted/50",
+      className,
+    )}
+    {...props}
+  />
+));
 TableBody.displayName = "TableBody";
 
 const TableFooter = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
   ({ className, ...props }, ref) => (
-    <tfoot ref={ref} className={cn("border-t bg-muted/50 font-medium [&>tr]:last:border-b-0", className)} {...props} />
+    <tfoot
+      ref={ref}
+      className={cn("border-t border-border bg-muted/50 font-medium [&>tr]:last:border-b-0", className)}
+      {...props}
+    />
   ),
 );
 TableFooter.displayName = "TableFooter";
@@ -34,7 +60,10 @@ const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTML
   ({ className, ...props }, ref) => (
     <tr
       ref={ref}
-      className={cn("border-b transition-colors data-[state=selected]:bg-muted hover:bg-muted/50", className)}
+      className={cn(
+        "border-b border-border transition-colors data-[state=selected]:bg-muted",
+        className,
+      )}
       {...props}
     />
   ),
@@ -46,7 +75,7 @@ const TableHead = React.forwardRef<HTMLTableCellElement, React.ThHTMLAttributes<
     <th
       ref={ref}
       className={cn(
-        "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+        "h-10 px-2 text-left align-middle font-medium text-muted-foreground last:text-right",
         className,
       )}
       {...props}
@@ -57,7 +86,11 @@ TableHead.displayName = "TableHead";
 
 const TableCell = React.forwardRef<HTMLTableCellElement, React.TdHTMLAttributes<HTMLTableCellElement>>(
   ({ className, ...props }, ref) => (
-    <td ref={ref} className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)} {...props} />
+    <td
+      ref={ref}
+      className={cn("px-2 py-2.5 align-middle last:text-right", className)}
+      {...props}
+    />
   ),
 );
 TableCell.displayName = "TableCell";
